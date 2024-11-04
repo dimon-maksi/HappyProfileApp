697d61cc448b567f45d1b6e7c90dc69cc4335372
MDS\maksy <maksymenko.ds.3@gmail.com>
2024-11-04 23:20:33 +0200

    feat: - create: -- main layout -- user profile phote -- user info fields -- chuck jokes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/toastr', express.static(path.join(__dirname, '../node_modules/toastr/build')));
 app.use('/jquery', express.static(path.join(__dirname, '../node_modules/jquery/dist')));
+app.use('/bootstrap', express.static(path.join(__dirname, '../node_modules/bootstrap/dist')));
+
+app.get('/api-url', (req, res) => {
+    res.json({ apiUrl: process.env.CHUCKAPI });
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
---
+++ src/public/js/home.js
@@ -0,0 +1,137 @@
+$(document).ready(function () {
+    if (localStorage.getItem('userInfo')) {
+        const userInfo = JSON.parse(
+            localStorage.getItem('userInfo')
+        );
+        $('#firstName').val(userInfo.firstName);
+        $('#lastName').val(userInfo.lastName);
+        $('#age').val(userInfo.age);
+        $('#email').val(userInfo.email);
+    }
+
+    function saveUserInfo(userInfo) {
+        localStorage.setItem('userInfo', JSON.stringify(userInfo));
+    }
+
+    function saveFirstName() {
+        const firstName = $('#firstName').val();
+        if (firstName) {
+            const userInfo =
+                JSON.parse(localStorage.getItem('userInfo')) || {};
+            userInfo.firstName = firstName;
+            saveUserInfo(userInfo);
+            toastr.success(
+                'First Name saved successfully!',
+                'Success'
+            );
+        } else {
+            toastr.error(
+                'Please enter a valid First Name.',
+                'Error'
+            );
+        }
+    }
+
+    function saveLastName() {
+        const lastName = $('#lastName').val();
+        if (lastName) {
+            const userInfo =
+                JSON.parse(localStorage.getItem('userInfo')) || {};
+            userInfo.lastName = lastName;
+            saveUserInfo(userInfo);
+            toastr.success(
+                'Last Name saved successfully!',
+                'Success'
+            );
+        } else {
+            toastr.error(
+                'Please enter a valid Last Name.',
+                'Error'
+            );
+        }
+    }
+
+    function saveAge() {
+        const age = $('#age').val();
+        if (age) {
+            const userInfo =
+                JSON.parse(localStorage.getItem('userInfo')) || {};
+            userInfo.age = age;
+            saveUserInfo(userInfo);
+            toastr.success('Age saved successfully!', 'Success');
+        } else {
+            toastr.error('Please enter a valid Age.', 'Error');
+        }
+    }
+
+    function saveEmail() {
+        const email = $('#email').val();
+        if (email) {
+            const userInfo =
+                JSON.parse(localStorage.getItem('userInfo')) || {};
+            userInfo.email = email;
+            saveUserInfo(userInfo);
+            toastr.success('Email saved successfully!', 'Success');
+        } else {
+            toastr.error('Please enter a valid Email.', 'Error');
+        }
+    }
+
+    $('#firstName').on('blur', saveFirstName);
+    $('#lastName').on('blur', saveLastName);
+    $('#age').on('blur', saveAge);
+    $('#email').on('blur', saveEmail);
+
+    $.get('/api-url', function (data) {
+        function fetchJoke() {
+            $.ajax({
+                url: data.apiUrl,
+                method: 'GET',
+                success: function (data) {
+                    $('#jokeContainer').text(data.value).show();
+                },
+                error: function () {
+                    toastr.error(
+                        'Failed to fetch a joke.',
+                        'Error'
+                    );
+                },
+            });
+        }
+
+        fetchJoke();
+
+        $('#fetchJoke').on('click', fetchJoke);
+    });
+});
+
+$(document).ready(function () {
+    const savedPhoto = localStorage.getItem('profilePhoto');
+    if (savedPhoto) {
+        $('#profilePhoto').attr('src', savedPhoto);
+    }
+
+    $('#fileInput').on('change', function (e) {
+        const file = e.target.files[0];
+        if (file && file.type === 'image/webp') {
+            const reader = new FileReader();
+            reader.onload = function (event) {
+                localStorage.setItem(
+                    'profilePhoto',
+                    event.target.result
+                );
+                $('#profilePhoto').attr('src', event.target.result);
+                toastr.success(
+                    'Profile photo uploaded successfully!',
+                    'Success'
+                );
+            };
+            reader.readAsDataURL(file);
+        } else {
+            toastr.error(
+                'Please upload a valid .webp image file.',
+                'Error'
+            );
+        }
+    });
+});
